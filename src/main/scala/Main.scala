0837132534642e410ef1e21afd01075aa76afbf1
rudy-esquerra <100610549+rudy-esquerra@users.noreply.github.com>
2022-03-29 14:48:37 -0700

    implemented case for creating and deleting users

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,8 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, when}
 
+import scala.Console.{BLUE, BOLD, RED, RESET, WHITE}
+
 
 
 object Main {
@@ -12,7 +14,7 @@ object Main {
 
     val spark = SparkSession
       .builder
-      .appName("hello hive")
+      .appName("SDBurrosApp")
       .config("spark.master", "local")
       .enableHiveSupport()
       .getOrCreate()
@@ -26,64 +28,170 @@ object Main {
         && spark.catalog.tableExists("credentials")
         && spark.catalog.tableExists("query_data")){
 
-      var cdfTable = spark.table("credentials").toDF()
-      var queryTable = spark.table("query_data").toDF()
+
 
       print("Enter username: ")
       val userInput = readLine()
       print("Enter password: ")
       val userPassword = readLine()
 
-      println("This should be the table credentials with the updated password")
-      spark.sql("select * from credentials").show()
+//      println("crentials table: ")
+//      spark.sql("select * from credentials").show()
 
       val checkUser = spark.sql(s"SELECT * FROM credentials WHERE username='${userInput}' AND password='${userPassword}'").count()
 
       if(checkUser > 0) {
-        userInput match {
-          case "admin" =>
-            println("Enter \n1 to create own query \n2 to read query from menu \n3 to update your password \n4 to delete a user")
-            val option = readInt()
-            option match {
-              case 1 => {
-                println("Entered option 1")
-                println("Type your customized query: ")
-                println("Dont forget to include \"...\" !!!")
-                val query = readLine()
-                spark.sql("create table IF NOT EXISTS temp(query String)")
-                spark.sql(s"INSERT INTO temp VALUES($query)")
-                val result = query.replaceAll("\"","")
-                spark.sql(s"${result}").show()
-                spark.sql("SELECT * FROM temp")
-              }
-              case 2 => {
-                print("Entered option 2")
+                  userInput match {
+                    case "admin" => {
+                      var continue = true
+                      while (continue) {
+                        var cdfTable = spark.table("credentials").toDF()
+                        var queryTable = spark.table("query_data").toDF()
+                        println(s"${BOLD}Enter \n1 => ${BOLD}${RED}C${RESET}reate Own Query " +
+                          s"\n2 => ${BOLD}${RED}R${RESET}ead Query From Menu " +
+                          s"\n3 => ${BOLD}${RED}U${RESET}pdate Your Password " +
+                          s"\n4 => ${BOLD}${RED}D${RESET}elete User " +
+                          s"\n5 => Add User" +
+                          s"\n'q' => Exit")
+                        val option = readLine()
+                        option match {
+                          case "1" => {
+                            println("Chose option 1")
+                            println("Type your customized query: ")
+                            println("Dont forget to include \"...\" !!!")
+                            val query = readLine()
+                            spark.sql("create table IF NOT EXISTS temp(query String)")
+                            spark.sql(s"INSERT INTO temp VALUES($query)")
+                            val result = query.replaceAll("\"", "")
+                            spark.sql(s"${result}").show()
+                            spark.sql("SELECT * from temp").show()
+                          }
+                          case "2" => {
+                            println("Chose option 2")
+                            println("Choose your desired query: " +
+                              "\n1. What restaurant has the less expensive burrito with the best yelp review." +
+                              "\n2. Which top three neighborhoods offer the burritos with google and yelp reviews combined with an average of 4.5+" +
+                              "\n3. What are the top five burrito restaurants where the combination of meat and salsa average 4+" +
+                              "\n4. What is the place with the burrito has an overall qualification by the reviewer of 4+ which includes pico de gallo an guac" +
+                              "\n5. What is the average cost of a burrito in the Hillcrest neighborhood." +
+                              "\n6. What type of meatless burrito has the worst overall qualifications.")
+                            val option = readInt()
+                            option match {
+                              case 1 => {
+                                println("The restaurant with the less expensive burrito and best yelp review is: ")
+                                spark.sql("SELECT DISTINCT location, btype, neighborhood, yelp, google, cost FROM burritos_data WHERE yelp > 4 AND neighborhood !='Houston' ORDER BY cost ASC LIMIT 5").show()
+                              }
+                              case 2 => {
+                                println("The top three neighborhoods that offer the burritos with google and yelp reviews combined with an average of 4.5+ are: ")
+                                spark.sql("SELECT DISTINCT bavg.location, bavg.average, btotal.neighborhood FROM burritos_location bavg LEFT JOIN burritos_data btotal ON (bavg.id_Number = btotal.id) ORDER BY average DESC LIMIT 3").show(200, false)
+                              }
+                              case 3 => {
+                                println("The top five burrito restaurants where the combination of meat and salsa average 4+ are: ")
+                                spark.sql("SELECT location, btype, neighborhood, meat, salsa_quality FROM burritos_data WHERE meat > 4 AND salsa_quality > 4 ORDER BY meat DESC LIMIT 5").show(false)
+                              }
+                              case 4 => {
+                                println("The place with the burrito that has an overall qualification by the reviewer of 4+ which includes pico de gallo an guac is: ")
+                                spark.sql("SELECT DISTINCT location, btype, neighborhood, overall, pico, guac FROM burritos_data WHERE overall > 4 AND pico='x' AND guac='x' AND UPPER(location) NOT LIKE UPPER('%Taco Stand%') ORDER BY overall DESC").show(false)
+                              }
+                              case 5 => {
+                                println("The average cost of a burrito in the Hillcrest neighborhood is: ")
+                                spark.sql("SELECT AVG(cost) FROM burritos_data WHERE neighborhood='Hillcrest'").show(false)
+                              }
+                              case 6 => {
+                                println("The meatless burrito with the worst overall qualifications is: ")
+                                spark.sql("SELECT location, btype, neighborhood, cost, overall FROM burritos_data WHERE beef='' AND pork='' AND chicken='' AND shrimp='' AND fish='' AND overall IS NOT NULL ORDER BY overall ASC").show(false)
+                              }
+                            }
+                          }
+                          case "3" => {
+                            println("Chose option 3")
+                            print("Enter a new password: ")
+                            val newPassword = readLine()
 
-              }
-              case 3 => {
-                println("Entered option 3")
-                print("Enter a new password: ")
-                val newPassword = readLine()
+                            val updatedCdf = cdfTable.withColumn("password", when(col("password") === s"$userPassword", s"$newPassword").otherwise(col("password")))
+                            updatedCdf.show()
+                            updatedCdf.write.mode("overwrite").saveAsTable("credentials2")
+                            spark.sql("DROP TABLE IF EXISTS credentials")
+                            spark.sql("ALTER TABLE credentials2 RENAME TO credentials")
+                          }
+                          case "4" => {
+                            println("Chose option 4")
+                            println("Enter username you want to delete")
+                            val delete = readLine()
+                            var cdfTableFiltered = cdfTable.filter(!col("username").isin(s"$delete"))
+                            cdfTableFiltered.show()
+                            cdfTableFiltered.write.mode("overwrite").saveAsTable("credentials3")
+                            spark.sql("DROP TABLE IF EXISTS credentials")
+                            spark.sql("ALTER TABLE credentials3 RENAME TO credentials")
+                          }
+                          case "5" => {
+                            println("Chose option 5")
+                            println("Type the username: ")
+                            println("Dont forget to include \"...\" !!!")
+                            val newUser = readLine()
+                            println("Type the password: ")
+                            val newPass = readLine()
+                            spark.sql("DROP TABLE IF EXISTS adduser")
+                            spark.sql("create table IF NOT EXISTS adduser(username String, password String)")
+                            spark.sql(s"INSERT INTO adduser VALUES($newUser, $newPass)")
+                            var cdfAdd = spark.table("adduser").toDF()
+                            cdfTable.union(cdfAdd).write.mode("append").saveAsTable("credentials4")
+                            spark.sql("DROP TABLE IF EXISTS credentials")
+                            spark.sql("ALTER TABLE credentials4 RENAME TO credentials")
+                            spark.sql("SELECT * FROM credentials").show()
+                          }
+                          case "q" => continue = false
+                          case _ => println(s"${BOLD}${RED}ERROR:${WHITE} Unknown command ${RESET}")
+                        }
+                      }
+                      println(s"\n${BOLD}${BLUE}See ya!\n")
+                    }
+                    case _ =>
+                      var continue = true
+                      while (continue) {
+                        println("Choose your desired query: " +
+                          "\n1 => What restaurant has the less expensive burrito with the best yelp review." +
+                          "\n2 => Which top three neighborhoods offer the burritos with google and yelp reviews combined with an average of 4.5+" +
+                          "\n3 => What are the top five burrito restaurants where the combination of meat and salsa average 4+" +
+                          "\n4 => What is the place with the burrito has an overall qualification by the reviewer of 4+ which includes pico de gallo an guac" +
+                          "\n5 => What is the average cost of a burrito in the Hillcrest neighborhood." +
+                          "\n6 => What type of meatless burrito has the worst overall qualifications." +
+                          "\n'q' => Exit ")
+                        val option = readLine()
+                        option match {
+                          case "1" => {
+                            println("The restaurant with the less expensive burrito and best yelp review is: ")
+                            spark.sql("SELECT DISTINCT location, btype, neighborhood, yelp, google, cost FROM burritos_data WHERE yelp > 4 AND neighborhood !='Houston' ORDER BY cost ASC LIMIT 5").show()
+                          }
+                          case "2" => {
+                            println("The top three neighborhoods that offer the burritos with google and yelp reviews combined with an average of 4.5+ are: ")
+                            spark.sql("SELECT DISTINCT bavg.location, bavg.average, btotal.neighborhood FROM burritos_location bavg LEFT JOIN burritos_data btotal ON (bavg.id_Number = btotal.id) ORDER BY average DESC LIMIT 5").show(200, false)
+                          }
+                          case "3" => {
+                            println("The top five burrito restaurants where the combination of meat and salsa average 4+ are: ")
+                            spark.sql("SELECT location, btype, neighborhood, meat, salsa_quality FROM burritos_data WHERE meat > 4 AND salsa_quality > 4 ORDER BY meat DESC").show(false)
+                          }
+                          case "4" => {
+                            println("The place with the burrito that has an overall qualification by the reviewer of 4+ which includes pico de gallo an guac is: ")
+                            spark.sql("SELECT DISTINCT location, btype, neighborhood, overall, pico, guac FROM burritos_data WHERE overall > 4 AND pico='x' AND guac='x' AND UPPER(location) NOT LIKE UPPER('%Taco Stand%') ORDER BY overall DESC").show(false)
+                          }
+                          case "5" => {
+                            println("The average cost of a burrito in the Hillcrest neighborhood is: ")
+                            spark.sql("SELECT AVG(cost) FROM burritos_data WHERE neighborhood='Hillcrest'").show(false)
+                          }
+                          case "6" => {
+                            println("The meatless burrito with the worst overall qualifications is: ")
+                            spark.sql("SELECT location, btype, neighborhood, cost, overall FROM burritos_data WHERE beef='' AND pork='' AND chicken='' AND shrimp='' AND fish='' AND overall IS NOT NULL ORDER BY overall ASC").show(false)
+                          }
+                          case "q" => continue = false
+                        }
+                      }
+                      println(s"\n${BOLD}${BLUE}See ya!\n")
+                  }
 
-                val updatedCdf = cdfTable.withColumn("password", when(col("password") === s"$userPassword", s"$newPassword").otherwise(col("password")))
-                updatedCdf.show()
-                updatedCdf.write.mode("overwrite").saveAsTable("credentials2")
-                spark.sql("DROP TABLE IF EXISTS credentials")
-                spark.sql("ALTER TABLE credentials2 RENAME TO credentials")
-              }
-              case 4 => {
-                print("Entered option 4")
-
-              }
-              case _ =>
-                println("Choose 1 thru 6 queries")
-            }
-          case _ =>
-        }
-      } else println("Incorrect user or password")
-
-
-    } else {
+      }
+        else println(s"${BOLD}${RED}Incorrect User or Password!${RESET}")
+      } else {
           var cdf = spark.read.option("header", true).csv("credentials.csv") //created schema reading from a csv file
           .withColumnRenamed("_c0","username")
           .withColumnRenamed("_c1","password")
@@ -102,23 +210,7 @@ object Main {
 
 
 
-//    println("What restaurant has the less expensive burrito with the best yelp review.")
-//    spark.sql("SELECT location, btype, neighborhood, yelp, google, cost FROM burritos_data WHERE yelp > 4 AND neighborhood !='Houston' ORDER BY cost ASC LIMIT 5").show()
 
-//    println("Which top three neighborhoods offer the burritos with google and yelp reviews combined with an average of 4.5+")
-//    spark.sql("SELECT DISTINCT bavg.location, bavg.average, btotal.neighborhood FROM burritos_location bavg LEFT JOIN burritos_data btotal ON (bavg.id_Number = btotal.id) ORDER BY average DESC LIMIT 5").show(200 , false)
-
-//    println("What are the top five burrito restaurants where the combination of meat and salsa average 4+")
-//    spark.sql("SELECT location, btype, neighborhood, meat, salsa_quality FROM burritos_data WHERE meat > 4 AND salsa_quality > 4 ORDER BY meat DESC").show(false)
-
-//    println("What is the place with the burrito has an overall qualification by the reviewer of 4+ which includes pico de gallo an guac")
-//    spark.sql("SELECT DISTINCT location, btype, neighborhood, overall, pico, guac FROM burritos_data WHERE overall > 4 AND pico='x' AND guac='x' AND UPPER(location) NOT LIKE UPPER('%Taco Stand%') ORDER BY overall DESC").show(false)
-
-//    println("What is the average cost of a burrito in a particular neighborhood.")
-//    spark.sql("SELECT AVG(cost) FROM burritos_data WHERE neighborhood='Hillcrest'").show(false)
-
-//    println("What type of meatless burrito has the worst overall qualifications.")
-//    spark.sql("SELECT location, btype, neighborhood, cost, overall FROM burritos_data WHERE beef='' AND pork='' AND chicken='' AND shrimp='' AND fish='' AND overall IS NOT NULL ORDER BY overall ASC").show(false)
 
   }
 }
